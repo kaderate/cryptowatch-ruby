16e44cd6d57b455617b7a53d7fa6da7d0dd67211
Esteban <gonzal_e@etna-alternance.net>
2017-10-30 20:05:19 +0100

    Initial commit

---
+++ cryptowatch/cryptowatch/lib/api/version.rb
@@ -0,0 +1,13 @@
+# @Author: Esteban GONZALEZ <Esteban>
+# @Date:   Tuesday, October 17th 2017, 9:36:14 pm
+# @Email:  gonzal_e@etna-alternance.net
+# @Project: RubyCryptowatch
+# @Last modified by:   Esteban
+# @Last modified time: Tuesday, October 17th 2017, 11:10:58 pm
+
+module Cryptowatch
+  module Api
+    class Version  
+    end
+  end
+end
---
+++ cryptowatch/cryptowatch/lib/version.rb
@@ -0,0 +1,11 @@
+# @Author: Esteban GONZALEZ <Esteban>
+# @Date:   Tuesday, October 17th 2017, 9:38:57 pm
+# @Email:  gonzal_e@etna-alternance.net
+# @Project: RubyCryptowatch
+# @Last modified by:   Esteban
+# @Last modified time: Tuesday, October 17th 2017, 11:10:53 pm
+
+module Cryptowatch
+  VERSION = [0, 0, 0, 1]
+  VERSION_FORMAT = VERSION.join('.')
+end
