d7f78996c5aefb9cd3be20596e57b0c21c51dc0a
Esteban <gonzal_e@etna-alternance.net>
2017-10-31 10:34:56 +0100

    Structure updated

---
+++ cryptowatch/lib/cryptowatch.rb
@@ -0,0 +1,11 @@
+# @Author: Esteban GONZALEZ <Esteban>
+# @Date:   Tuesday, October 17th 2017, 8:39:43 pm
+# @Email:  gonzal_e@etna-alternance.net
+# @Project: RubyCryptowatch
+# @Last modified by:   esteban
+# @Last modified time: Tuesday, October 31st 2017, 10:08:37 am
+
+module Cryptowatch
+  include '/lib/api/api'
+  include '/lib/api/version'
+end
--- cryptowatch/tests/request_test.rb
+++
@@ -1,8 +0,0 @@
-# @Author: Esteban GONZALEZ <Esteban>
-# @Date:   Tuesday, October 17th 2017, 9:09:46 pm
-# @Email:  gonzal_e@etna-alternance.net
-# @Project: RubyCryptowatch
-# @Last modified by:   esteban
-# @Last modified time: Monday, October 30th 2017, 8:04:43 pm
-
-require '../cryptowatch/cryptowatch'
