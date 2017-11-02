5e1e38a94d88e66fe9a3b1a8ab780ee7ed5692ae
Esteban <gonzal_e@etna-alternance.net>
2017-11-02 19:28:18 +0100

    add module client

--- cryptowatch-ruby/lib/cryptowatch.rb
+++ cryptowatch-ruby/lib/cryptowatch.rb
@@ -3,7 +3,7 @@
 # @Email:  gonzal_e@etna-alternance.net
 # @Project: RubyCryptowatch
 # @Last modified by:   esteban
-# @Last modified time: Thursday, November 2nd 2017, 6:47:43 pm
+# @Last modified time: Thursday, November 2nd 2017, 7:26:25 pm
 
 require "rubygems"
 
