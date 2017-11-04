e230c630e731f6097916da5b49c6626c4b70dfd8
Esteban <gonzal_e@etna-alternance.net>
2017-11-04 23:20:52 +0100

    old structure deleted

--- cryptowatch/README.md
+++
@@ -1,7 +0,0 @@
-# Cryptowatch Library for Ruby
-
-This is will be: *a simple interface in Ruby for Cryptowatch.*
-
-I'm developing **my first library** in Ruby in parallel to my learning of this language.
-
-Cryptowat.ch API: https://cryptowat.ch/docs/
--- cryptowatch/tests/test_bitcoin_price.rb
+++
@@ -1,6 +0,0 @@
-# @Author: Esteban GONZALEZ <esteban>
-# @Date:   Tuesday, October 31st 2017, 10:29:25 am
-# @Email:  gonzal_e@etna-alternance.net
-# @Project: RubyCryptowatch
-# @Last modified by:   esteban
-# @Last modified time: Tuesday, October 31st 2017, 10:29:25 am
